e0a76bf7f0b05a1cf76d5dbc19f3bb70a001817d
Silverpoision <piyush1671@gmail.com>
2023-01-30 14:25:18 +0530

    .

--- index.js
+++ index.js
@@ -65,6 +65,6 @@ app.use((err, req, res, next) => {
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
   });
 });
